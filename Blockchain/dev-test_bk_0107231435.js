1502e3233245d85d84af4b1222741ba998e72823
AlfredoLimon <4.limon@gmail.com>
2023-01-11 22:50:02 -0600

    p2p server part 20

--- Blockchain/dev-test_bk_0107231435.js
+++
@@ -1,5 +0,0 @@
-const Block = require('./block');
-const block = new Block('07-01-2023', "0".repeat(64), '0'.repeat(64), "Datos a poner aca");
-
-console.log(block.toString());
-console.log(Block.genesis().toString());
--- Blockchain/dev-test_bk_0107231459.js
+++
@@ -1,6 +0,0 @@
-const Block = require('./block');
-
-for (let i = 1; i<=100; i++){
-    block = new Block(Date.now(), "0".repeat(64), "0".repeat(64), `Data${i}`);    
-    console.log(block.toString());
-}
